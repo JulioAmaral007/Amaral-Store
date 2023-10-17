78a0a475673be1471c2668b0a0fea230a635430b
Julio Cezar <captaingamergroot@gmail.com>
2023-10-16 22:21:40 -0300

    feat: add header menu using sheet component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,14 +1,61 @@
-import { MenuIcon, ShoppingCartIcon } from 'lucide-react'
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from 'lucide-react'
 import { Button } from './button'
 import { Card } from './card'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from './sheet'
 
 export function Header() {
   return (
     <div>
       <Card className="flex justify-between p-[1.875rem] items-center">
-        <Button size="icon" variant="outline">
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle className="text-left text-lg font-semibold">
+                Menu
+              </SheetTitle>
+            </SheetHeader>
+
+            <div className="mt-2 flex flex-col gap-2">
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <LogInIcon size={16} />
+                Fazer login
+              </Button>
+
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <HomeIcon size={16} />
+                Início
+              </Button>
+
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <PercentIcon size={16} />
+                Ofertas
+              </Button>
+
+              <Button variant="outline" className="w-full justify-start gap-2">
+                <ListOrderedIcon size={16} />
+                Catálogo
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
 
         <h1 className="font-semibold text-lg">
           <span className="text-primary">KX</span> Store
