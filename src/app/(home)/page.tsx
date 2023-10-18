be5b78477885cecd15c60d496dcbf96c3a09a19b
Júlio Cézar <captaingamergroot@gmail.com>
2023-10-18 10:52:50 -0300

    feat: add third PromoBanner and mouses section to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -20,18 +20,26 @@ export default async function Home() {
       },
     },
   })
+
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: 'mouses',
+      },
+    },
+  })
   return (
-    <div className="">
+    <div className="flex flex-col gap-8">
       <PromoBanner
         src="/banner-home-01.png"
         alt="Até 55% de desconto em mouses!"
       />
 
-      <div className="mt-8 px-5">
+      <div className="px-5">
         <Categories />
       </div>
 
-      <div className="my-8">
+      <div>
         <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
@@ -41,9 +49,19 @@ export default async function Home() {
         alt="Até 55% de desconto em mouses!"
       />
 
-      <div className="my-8">
+      <div>
         <SectionTitle>Teclados</SectionTitle>
         <ProductList products={keyboards} />
+      </div>
+
+      <PromoBanner
+        src="/banner-home-03.png"
+        alt="Até 20% de desconto em fones!"
+      />
+
+      <div>
+        <SectionTitle>Mouses</SectionTitle>
+        <ProductList products={mouses} />
       </div>
     </div>
   )
