ecc8c73f6b4b36107773091627be8d0dcf8e85c8
Julio Cezar <captaingamergroot@gmail.com>
2023-10-18 19:48:19 -0300

    chore: adjust in width and height

--- src/components/ui/productItem.tsx
+++ src/components/ui/productItem.tsx
@@ -12,41 +12,44 @@ export function ProductItem({ product }: ProductItemProps) {
     <div className="flex flex-col gap-4">
       <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
         <Image
-          className="h-auto w-auto max-w-[80%] max-h-[70%]"
-          sizes="100vw"
           src={product.imageUrls[0]}
-          alt={product.name}
-          width={0}
           height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%]"
           style={{
             objectFit: 'contain',
           }}
+          alt={product.name}
         />
 
         {product.discountPercent > 0 && (
-          <Badge className="absolute left-3 top-3 px-2 py-[2px]">
+          <Badge className="absolute left-3 top-3 px-2 py-[2px] ">
             <ArrowDown size={14} /> {product.discountPercent}%
           </Badge>
         )}
       </div>
 
       <div className="flex flex-col gap-1">
-        <p className="text-sm overflow-hidden whitespace-nowrap text-ellipsis">
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
           {product.name}
         </p>
 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 ">
           {product.discountPercent > 0 ? (
             <>
-              <p className="font-semibold">
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
                 R$ {product.totalPrice.toFixed(2)}
               </p>
-              <p className="opacity-75 line-through text-xs">
+
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
                 R$ {Number(product.basePrice).toFixed(2)}
               </p>
             </>
           ) : (
-            <p className="font-semibold">R$ {product.basePrice.toFixed(2)}</p>
+            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
+              R$ {product.basePrice.toFixed(2)}
+            </p>
           )}
         </div>
       </div>
