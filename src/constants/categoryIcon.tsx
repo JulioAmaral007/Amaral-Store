b219db2b9d02ce890f5c4d9cb585682508b6677b
Julio Cezar <captaingamergroot@gmail.com>
2023-10-20 10:59:56 -0300

    style: add responsiveness for larger screens in the categories component

--- src/constants/categoryIcon.tsx
+++ src/constants/categoryIcon.tsx
@@ -4,14 +4,14 @@ import {
   MonitorIcon,
   MouseIcon,
   SpeakerIcon,
-  SquareIcon,
+  SquareDashedBottom
 } from 'lucide-react'
 
 export const CATEGORY_ICON = {
   keyboards: <KeyboardIcon size={16} />,
   monitors: <MonitorIcon size={16} />,
   headphones: <HeadphonesIcon size={16} />,
-  mousepads: <SquareIcon size={16} />,
+  mousepads: <SquareDashedBottom size={16} />,
   speakers: <SpeakerIcon size={16} />,
   mouses: <MouseIcon size={16} />,
 }
