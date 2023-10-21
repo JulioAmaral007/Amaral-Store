0ac085371a0cc058cde21743ce564db7eec345aa
Julio Cezar <captaingamergroot@gmail.com>
2023-10-21 09:14:44 -0300

    style: add responsiveness for larger screens in the catalog page

--- src/app/catalog/components/categoryItem.tsx
+++ src/app/catalog/components/categoryItem.tsx
@@ -10,17 +10,14 @@ export function CategoryItem({ category }: CategoryItemProps) {
   return (
     <Link href={`/category/${category.slug}`}>
       <div className="flex flex-col">
-        <div className="bg-category-item-gradient flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg">
+        <div className="bg-category-item-gradient flex h-[150px] lg:h-[255px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg">
           <Image
             src={category.imageUrl}
             alt={category.name}
             width={0}
             height={0}
             sizes="100vw"
-            className="h-auto max-h-[70%] w-auto max-w-[80%]"
-            style={{
-              objectFit: 'contain',
-            }}
+            className="h-auto max-h-[70%] min-w-[40%] max-w-[80%]"
           />
         </div>
 
--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -6,16 +6,16 @@ import { CategoryItem } from './components/categoryItem'
 export default async function Catalog() {
   const categories = await prismaClient.category.findMany({})
   return (
-    <div className="flex flex-col gap-8 p-5">
+    <div className="flex flex-col gap-8 p-5 lg:container lg:mx-auto">
       <Badge
-        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase lg:w-1/6 lg:justify-center lg:py-2"
         variant="outline"
       >
         <Shapes size={16} />
         Catálogo
       </Badge>
 
-      <div className="grid grid-cols-2 gap-8">
+      <div className="grid grid-cols-2 gap-8 lg:grid-cols-3">
         {categories.map((category) => (
           <CategoryItem key={category.id} category={category} />
         ))}
