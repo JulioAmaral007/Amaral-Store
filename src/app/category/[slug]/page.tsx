8fa664f91d24606700c2a0cda7d9d28ef3258f5b
Julio Cezar <captaingamergroot@gmail.com>
2023-10-21 09:53:54 -0300

    style: add responsiveness for larger screens in the category page

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -19,16 +19,16 @@ export default async function CategoryProducts({ params }: any) {
     return null
   }
   return (
-    <div className="flex flex-col gap-8 p-5">
+    <div className="flex flex-col gap-8 p-5 lg:container lg:mx-auto">
       <Badge
-        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase lg:w-1/6 lg:justify-center lg:py-2"
         variant="outline"
       >
         {CATEGORY_ICON[params.slug as keyof typeof CATEGORY_ICON]}
         {category.name}
       </Badge>
 
-      <div className="grid grid-cols-2 gap-8">
+      <div className="grid grid-cols-2 gap-8 md:grid-cols-4 lg:grid-cols-6 lg:gap-10">
         {category.products.map((product) => (
           <ProductItem
             key={product.id}
