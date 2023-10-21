af7116c04d8d24c3f6ccddee4f3526cec4914869
Julio Cezar <captaingamergroot@gmail.com>
2023-10-21 09:55:42 -0300

    style: add responsiveness for larger screens in the offers page

--- src/app/offers/page.tsx
+++ src/app/offers/page.tsx
@@ -15,14 +15,14 @@ export default async function Offers() {
   return (
     <div className="flex flex-col gap-8 p-5">
       <Badge
-        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase lg:w-1/6 lg:justify-center lg:py-2"
         variant="outline"
       >
         <PercentIcon size={16} />
         Ofertas
       </Badge>
 
-      <div className="grid grid-cols-2 gap-8">
+      <div className="grid grid-cols-2 gap-8 md:grid-cols-4 lg:grid-cols-6 lg:gap-10">
         {deals.map((deal) => (
           <ProductItem key={deal.id} product={computeProductTotalPrice(deal)} />
         ))}
