03668d79025fbc8249ec6b3b54867792d7904d46
Julio Cezar <captaingamergroot@gmail.com>
2023-11-01 21:43:43 -0300

    feat: ensure category page is responsive on desktop

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -20,13 +20,13 @@ export default async function CategoryProducts({ params }: any) {
   }
 
   return (
-    <div className="flex flex-col gap-8 p-5 lg:container lg:mx-auto">
+    <div className="mx-auto flex flex-col gap-8 p-5 lg:container lg:gap-10 lg:py-10">
       <Badge variant="heading">
         {CATEGORY_ICON[params.slug as keyof typeof CATEGORY_ICON]}
         {category.name}
       </Badge>
 
-      <div className="grid grid-cols-2 gap-8 md:grid-cols-4 lg:grid-cols-6 lg:gap-10">
+      <div className="grid grid-cols-2 gap-8 lg:grid-cols-6">
         {category.products.map((product) => (
           <ProductItem
             key={product.id}
