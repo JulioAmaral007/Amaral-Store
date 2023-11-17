6adf57819eb278a754213339c2794ca7bdfb1c25
Julio Cezar <captaingamergroot@gmail.com>
2023-11-17 16:06:13 -0300

    feat: ensure products are fetched on products page

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -1,3 +1,7 @@
-export function ProductsPage() {
-  return <h1>Products</h1>
+import { prismaClient } from '@/lib/prisma'
+
+export async function ProductsPage() {
+  const products = await prismaClient.product.findMany()
+
+  return <h1>products</h1>
 }
